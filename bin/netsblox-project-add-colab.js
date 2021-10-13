709714b66db070db13cb88ea8f996d6f82a5463f
Adam <amgaweda@gmail.com>
2021-10-13 13:35:44 -0400

    Hardcoding to debug

--- bin/netsblox-project-add-colab.js
+++ bin/netsblox-project-add-colab.js
@@ -43,9 +43,9 @@ function callThis(aa){
     });
 }
 
-// const owner = "t01"
-// const projectName = "MyCoolProject3"
-// const collaborator = "s07"
+const owner = "test01"
+const projectName = "p01_activity2"
+const collaborator = "test01s1"
 
 storage.connect()
     // .then(() => callThis(collaborator))
